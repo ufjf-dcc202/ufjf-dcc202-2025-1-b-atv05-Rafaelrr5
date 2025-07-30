3d78aabcba3b1b87bf132ddb77087ba72a9170ce
Rafaelrr5 <faelrribeiro3@gmail.com>
2025-07-30 17:20:52 -0300

    adiciona estado na tela

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
+import { getTabuleiro } from "./discos.js";
+
 const eH1 = document.querySelector("h1")
 eH1.textContent = "Olá mundo!"
 
 const eTabuleiro = criaTabuleiro();
 document.body.append(eTabuleiro);
 
-for(let i = 0; i<7; i++){
-    const eDisco = criaDisco();
-    eTabuleiro.append(eDisco);
+const tabuleiro = getTabuleiro();
 
-    if(i<3) eDisco.dataset.cor = "branco";
-    else if(i>3) eDisco.dataset.cor = "preto";
-    else eDisco.dataset.cor = "transparente"
+for(let i = 0; i<7; i++){
+    const eDisco = criaDisco(tabuleiro[i]);
+    
+    eTabuleiro.append(eDisco);
 }
 
 function criaTabuleiro(){
     const novoTabuleiro = document.createElement("div");
+    
     novoTabuleiro.classList.add("tabuleiro");
     
     return novoTabuleiro;
 }
 
-function criaDisco(){
+function criaDisco(cor){
     const novoDisco = document.createElement("div");
-    novoDisco.classList.add("disco");
     
+    novoDisco.classList.add("disco");
+    novoDisco.dataset.cor = cor;
+
     return novoDisco;
 }
