d8db0b543f35df2885a403fad8682c13f0b96e20
Rafaelrr5 <faelrribeiro3@gmail.com>
2025-07-30 16:48:03 -0300

    adiciona criaDiscos

--- main.js
+++ main.js
@@ -4,8 +4,21 @@ eH1.textContent = "Olá mundo!"
 const eTabuleiro = criaTabuleiro();
 document.body.append(eTabuleiro);
 
+for(let i = 0; i<7; i++){
+    const eDisco = criaDisco();
+    eTabuleiro.append(eDisco);
+}
+
 function criaTabuleiro(){
     const novoTabuleiro = document.createElement("div");
     novoTabuleiro.classList.add("tabuleiro");
+    
     return novoTabuleiro;
+}
+
+function criaDisco(){
+    const novoDisco = document.createElement("div");
+    novoDisco.classList.add("disco");
+    
+    return novoDisco;
 }
